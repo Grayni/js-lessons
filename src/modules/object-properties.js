a84dd7ae33f7911e11a42f49bc897d0bbe6cbc9a
grayni <omenbestg@gmail.com>
2021-03-28 15:04:42 +0300

    comparing objs

--- src/modules/object-properties.js
+++ src/modules/object-properties.js
@@ -1,5 +1,20 @@
 function objectProperties() {
-  console.log('test')
+  let obj1 = {
+    name: 'Uriy',
+    age: '29'
+  }
+
+  let obj2 = {
+    name: 'Uriy',
+    age: '29'
+  }
+
+  // 1)
+  console.log(obj1 === obj2) // false obj unique
+
+  // 2)
+  obj1 = obj2 // assignment
+  console.log(obj1 === obj2) // obj1 reference on obj2
 }
 
 export {objectProperties}
