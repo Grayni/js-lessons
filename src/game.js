e9ff173816749ba34d81471096954eab714a190a
grayni <omenbestg@gmail.com>
2021-04-01 03:05:24 +0300

    finish create mini-game

--- src/game.js
+++ src/game.js
@@ -1,18 +1,46 @@
 
 const $start = document.querySelector('#start')
 const $game = document.querySelector('#game')
+let $time = document.querySelector('#time')
+const $inputTime = document.querySelector('#game-time')
+let $result = document.querySelector('#result-header')
+let counter = 0
 
 $start.addEventListener('click', gameStart)
 
+$inputTime.addEventListener('input', setValue)
+
+function setValue() {
+  $time.textContent = parseInt($inputTime.value).toFixed(1)
+}
 
 function gameStart(event) {
   $game.style.backgroundColor = '#fff'
+  $result.classList.add('hide')
   event.target.style.display = 'none'
   renderBox()
+  timer(event)
 }
 
-function gameEnd() {
+function gameEnd(event) {
+  $game.style.backgroundColor = '#ccc'
+  event.target.style.display = 'block'
+  $game.children[0].remove()
+  $time.textContent = parseInt($inputTime.value).toFixed(1)
+  $result.classList.remove('hide')
+  $result.children[0].textContent = counter
+  counter = 0
+  return 
+}
 
+function timer(event) {
+  const runTimer = setInterval(() => {
+    $time.textContent = parseFloat($time.textContent - .1).toFixed(1)
+    if (parseFloat($time.textContent)<=0) {
+      clearInterval(runTimer)
+      return gameEnd(event)
+    }
+  }, 100)
 }
 
 function renderBox() {
@@ -41,6 +69,12 @@ function renderBox() {
 function boxFix(event) {
   event.target.remove()
   renderBox()
+  countClick()
+}
+
+function countClick() {
+  counter++
+  console.log(counter)
 }
 
 function valueRandom(min, max) {
